a83e6cae60aefee6528077d6d1d2d6e13c8c8e04
p3tapio <p_tapio@yahoo.com>
2021-04-19 22:29:54 +0300

    ..

--- src/components/Selitysta.jsx
+++ src/components/Selitysta.jsx
@@ -5,7 +5,7 @@ const Selitysta = () => {
     <div>
       <h4>SELITYS</h4>
       <p>Muokattua selitystä ennen travisin kautta tehtyä deployta</p>
-      <p>JEEES, will this trigger travis?</p>
+      <p>JEEES, will this trigger travis #8?</p>
     </div>
   );
 };
